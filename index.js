42c60539a690ec2ecfee4a61b43c3ed5f40d92a8
awais-ur-rehman <awaisjarral37@gmail.com>
2024-06-01 06:42:43 +0500

    updated index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 8080;
+const port = 8080;
 
 // Middleware
 app.use(cors());
