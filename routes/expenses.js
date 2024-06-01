3e81d42f2494e0f937123c62e2040e6d9fe5d28a
awais-ur-rehman <awaisjarral37@gmail.com>
2024-06-01 06:06:02 +0500

    add filer api

--- routes/expenses.js
+++ routes/expenses.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Expense = require('../models/expense');
+const Expense = require("../models/expense");
 
 // API endpoint to fetch expenses
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const expenses = await Expense.find();
     res.json(expenses);
@@ -13,7 +13,7 @@ router.get('/', async (req, res) => {
 });
 
 // API endpoint to add a new expense
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const { date, category, amount, comment } = req.body;
   const newExpense = new Expense({ date, category, amount, comment });
 
@@ -22,6 +22,16 @@ router.post('/', async (req, res) => {
     res.status(201).json(newExpense);
   } catch (error) {
     res.status(400).json({ error: error.message });
+  }
+});
+
+// Route to get expenses with category "Rashan Exp."
+router.get("/rashan-exp", async (req, res) => {
+  try {
+    const expenses = await Expense.find({ category: "Rashan Exp." });
+    res.json(expenses);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 });
 
